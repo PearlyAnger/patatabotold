508a9672fbba2143cf5ccc3c2739f95bb2ba147e
Orhideon <43049721+Orhideon@users.noreply.github.com>
2019-11-10 17:33:04 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 bot.on("ready" , function(){
     bot.user.setGame("patatabot.com");
-    console.log("Le bot a bien ete connecte")
+    console.log("Le bot a bien ete connecte");
 });
 
  bot.login(process.env.TOKEN);
