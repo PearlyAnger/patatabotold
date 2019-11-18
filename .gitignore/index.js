9ae6c79e2bd0bd1b3acf8d9b741596cbb7a9699e
Orhideon <43049721+Orhideon@users.noreply.github.com>
2019-11-18 14:05:50 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const btc = require("btc-Value");
 
 var patata;
 var bot = new Discord.Client();
@@ -6,6 +7,7 @@ var name = ['Mathieu','Romain', 'Enzo'];
 
 bot.on("ready" , function(){
     bot.user.setActivity('patatabot.com', { type: 'WATCHING' });
+    console.log("Le bot a bien ete connecte")
     console.log('bot on');
 });
 
@@ -18,22 +20,18 @@ function rand(min, max)
 bot.on('message' , function(msg){
     if (msg.content === '!random'){
         msg.reply(name[rand(0,2)]);
-    
+
     }
 });
 
 
 bot.on('message' , function(msg){
 if (msg.content === '!patata'){
-    if(patata === undefined){
-        patata = rand(8,10);
-        console.log(patata);
-        msg.reply ('Le cours de la patata au G est redéfinie par Lolo à : ' + patata);
-
-    }else{
-        patata = patata + rand(1,2)/rand(1,2)+rand(-2,-1)/rand(2,1);
-    }
-    msg.reply (patata +' le G *mamène*');
+	
+    btc().then(value => {
+    msg.reply ( 0.001*value +' le G *mamène*');
+});
+    
 }
 });
 
